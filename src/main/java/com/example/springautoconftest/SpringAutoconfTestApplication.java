9f0ec3a4e7a938b402d3bd57cc33293034375eab
Min Zhu <zhumin@google.com>
2023-01-12 17:29:59 -0500

    manual tests on credentials and quotaprojectid.

--- src/main/java/com/example/springautoconftest/SpringAutoconfTestApplication.java
+++ src/main/java/com/example/springautoconftest/SpringAutoconfTestApplication.java
@@ -1,5 +1,8 @@
 package com.example.springautoconftest;
 
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.auth.oauth2.UserCredentials;
 import com.google.cloud.accessapproval.v1.AccessApprovalAdminClient;
 import com.google.cloud.accessapproval.v1.ApprovalRequest;
 import com.google.cloud.accessapproval.v1.ListApprovalRequestsMessage;
@@ -10,6 +13,7 @@ import com.google.cloud.assuredworkloads.v1.AssuredWorkloadsServiceClient;
 import com.google.cloud.assuredworkloads.v1.ListWorkloadsRequest;
 import com.google.cloud.assuredworkloads.v1.Workload;
 import com.google.cloud.functions.v2.Function;
+import com.google.cloud.functions.v2.FunctionName;
 import com.google.cloud.functions.v2.FunctionServiceClient;
 import com.google.cloud.functions.v2.ListFunctionsRequest;
 import com.google.cloud.language.v1.Document;
@@ -17,21 +21,24 @@ import com.google.cloud.language.v1.Document.Type;
 import com.google.cloud.language.v1.Entity;
 import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
+import com.google.cloud.spring.core.DefaultCredentialsProvider;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@ConfigurationProperties("application.properties")
+// @ConfigurationProperties("application.properties")
 public class SpringAutoconfTestApplication {
-
-  private static final String PROJECT_ID = System.getenv("SPRING_AUTOCONF_TEST_PROJECT_ID");
 
   @Autowired
   private LanguageServiceClient languageAutoClient;
@@ -47,6 +54,14 @@ public class SpringAutoconfTestApplication {
 
   @Autowired
   private AssuredWorkloadsServiceClient assuredWorkloadsAutoClient;
+
+  @Value( "${function-project}" )
+  private String project;
+
+  @Value("${function-name}")
+  private String functionName;
+
+  private static final Log LOGGER = LogFactory.getLog(SpringAutoconfTestApplication.class);
 
   // The text to analyze
   private String[] texts = {"I love this!", "I hate this!",
@@ -66,23 +81,24 @@ public class SpringAutoconfTestApplication {
 
   @GetMapping("/functions")
   public void syncListFunctions() throws Exception {
-      ListFunctionsRequest request =
-          ListFunctionsRequest.newBuilder()
-              .setParent(com.google.cloud.functions.v2.LocationName.of(PROJECT_ID, "us-central1").toString())
-              //.setPageSize(883849137)
-              //.setPageToken("pageToken873572522")
-              .build();
-      for (Function element :
-          functionsAutoClient.listFunctions(request).iterateAll()) {
-        System.out.println(element);
-      }
+    String quotaProjectId = this.functionsAutoClient.getSettings().getQuotaProjectId();
+    LOGGER.info("quotaProjectId set for Client library: " + quotaProjectId);
+    logCredentialId(this.functionsAutoClient.getSettings().getCredentialsProvider().getCredentials());
+    // in pantheon, enable service and create a function. then proceed
+    FunctionName name = FunctionName.of(project, "us-central1", functionName);
+    Function response = functionsAutoClient.getFunction(name);
+
+    LOGGER.info("Function name got: " + response.getName());
+    LOGGER.info("Function BuildConfig Runtime: " + response.getBuildConfig().getRuntime());
   }
 
   @GetMapping("/access-approval")
-  void withAccessApprovalConfigClient() {
+  void withAccessApprovalConfigClient() throws IOException {
+    logCredentialId(this.accessApprovalAutoClient.getSettings().getCredentialsProvider().getCredentials());
+
       ListApprovalRequestsMessage request =
           ListApprovalRequestsMessage.newBuilder()
-              .setParent(ProjectName.of(PROJECT_ID).toString())
+              .setParent(ProjectName.of(project).toString())
               //.setFilter("filter-1274492040")
               //.setPageSize(883849137)
               //.setPageToken("pageToken873572522")
@@ -90,20 +106,23 @@ public class SpringAutoconfTestApplication {
       int i = 0;
       for (ApprovalRequest element :
           accessApprovalAutoClient.listApprovalRequests(request).iterateAll()) {
-        System.out.println(element.toString());
+        LOGGER.info(element.toString());
       }
   }
 
   @GetMapping("/api-gateway")
-  void withApiGatewayConfigClient() throws Exception {
-      com.google.cloud.apigateway.v1.LocationName parent = com.google.cloud.apigateway.v1.LocationName.of(PROJECT_ID, "us-central1");
+  void withApiGatewayConfigClient() throws IOException {
+
+    logCredentialId(this.apiGatewayAutoClient.getSettings().getCredentialsProvider().getCredentials());
+      com.google.cloud.apigateway.v1.LocationName parent = com.google.cloud.apigateway.v1.LocationName.of(project, "us-central1");
       for (Gateway element : apiGatewayAutoClient.listGateways(parent).iterateAll()) {
-        System.out.println(element);
+        LOGGER.info(element);
       }
   }
 
   @GetMapping("/assured-workload")
-  void withAssuredWorkloadsConfigClient() {
+  void withAssuredWorkloadsConfigClient() throws IOException {
+    logCredentialId(this.assuredWorkloadsAutoClient.getSettings().getCredentialsProvider().getCredentials());
       ListWorkloadsRequest request =
           ListWorkloadsRequest.newBuilder()
               //.setParent(LocationName.of("[ORGANIZATION]", "[LOCATION]").toString())
@@ -121,9 +140,11 @@ public class SpringAutoconfTestApplication {
    * should add settings and credentials to show difference from client library directly
    */
   @GetMapping("/language")
-  void language() {
-
-     for (String text : this.texts) {
+  void language() throws IOException {
+    String quotaProjectId = this.languageAutoClient.getSettings().getQuotaProjectId();
+    LOGGER.info("quotaProjectId set for Client library: " + quotaProjectId);
+    logCredentialId(this.languageAutoClient.getSettings().getCredentialsProvider().getCredentials());
+    for (String text : this.texts) {
       Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
       // Detects the sentiment of the text
       Sentiment sentiment = this.languageAutoClient.analyzeSentiment(doc).getDocumentSentiment();
@@ -139,6 +160,21 @@ public class SpringAutoconfTestApplication {
           ", Salience: " + x.getSalience() +
           ", Sentiment: " + x.getSentiment() +
           ", Mention counts: " + x.getMentionsCount()));
+    }
+  }
+
+
+  private void logCredentialId(Credentials credentials) {
+
+    if (credentials instanceof UserCredentials) {
+      LOGGER.info(
+          "Default credentials provider for user "
+              + ((UserCredentials) credentials).getClientId());
+      LOGGER.info("user credential ProjectId: " + ((UserCredentials) credentials).getQuotaProjectId());
+    } else if (credentials instanceof ServiceAccountCredentials) {
+      LOGGER.info(
+          "Default credentials provider for service account "
+              + ((ServiceAccountCredentials) credentials).getClientEmail());
     }
   }
 }
